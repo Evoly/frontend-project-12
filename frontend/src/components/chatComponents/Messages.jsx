c6332ea9cb8ce0c2c257a056fab0d93f8ba57390
evoly <16191619+Evoly@users.noreply.github.com>
2025-04-21 16:09:04 +0700

    add scroll to messages

--- frontend/src/components/chatComponents/Messages.jsx
+++ frontend/src/components/chatComponents/Messages.jsx
@@ -1,12 +1,18 @@
+import { useRef, useEffect } from 'react';
 import { useTranslation } from 'react-i18next';
 
 const Messages = ({ messages, activeChannelId, channels }) => {
   const { t } = useTranslation();
+  const messagesRef = useRef(null);
+
+  useEffect(() => {
+    messagesRef.current?.lastChild?.scrollIntoView({ behavior: "smooth" });
+  }, [messages])
 
   const renderMessages = (id) => {
     const currentMessages = messages.filter((message) => message.channelId === id);
     const channel = channels.find((channel) => channel.id === id);
-    if (!channel) return null;
+    if (!channel) return null;  
 
     return (
       <>
@@ -18,7 +24,7 @@ const Messages = ({ messages, activeChannelId, channels }) => {
             {t('chatPage.messages', { count: currentMessages.length })}
           </span>
         </div>
-        <div id='messages-box' className='chat-messages overflow-auto px-5 '>
+        <div id='messages-box' className='chat-messages overflow-auto px-5 ' ref={messagesRef}>
           {currentMessages.map((message) => (
             <div className='text-break mb-2' key={message.id}>
               <b>{message.username}</b>: {message.body}
